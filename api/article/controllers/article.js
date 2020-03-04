7461f32e88a0f9ae3bfd0ceaa19d2d9cdb084446
mdykdan <mathis.dyk.pro@gmail.com>
2020-03-04 18:08:16 +0100

    api custom

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -10,6 +10,16 @@ module.exports = {
     return entities.map(entity => {
       let article = entity
 
+      if (entity.categories.length != 0) {
+        article.categories = []
+      }
+
+      entity.categories.forEach(element => {
+        article.categories.push({id: element.id, name: element.name})
+      });
+
+      delete article.categories
+
       if (article.disponible === true) {
         return article
       }
