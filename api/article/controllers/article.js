9e98380c7b1da61f494d32d05ff27539520bf14c
mdykdan <mathis.dyk.pro@gmail.com>
2020-03-05 11:32:27 +0100

    Fix filter

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,28 +1,35 @@
 module.exports = {
   async find(ctx) {
+    console.log(ctx.req)
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.article.search({ disponible: true});
+      entities = await strapi.services.article.search( ctx.query);
     } else {
-      entities = await strapi.services.article.find({disponible: true});
+      entities = await strapi.services.article.find( ctx.query);
     }
 
-    return entities.map(entity => {
+    return entities
+    .filter(element => {
+      if (element.disponible === true) {
+        return true
+      } else {
+        return false
+      }
+    })
+    .map(entity => {
       let article = entity
 
       if (entity.categories.length != 0) {
         article.categories = []
       }
 
-      entity.categories.forEach(element => {
-        article.categories.push({id: element.id, name: element.name})
-      });
+      entity.categories.map(element => {
+          article.categories.push({id: element.id, name: element.name})
+        });
 
-      delete article.categories
 
-      if (article.disponible === true) {
-        return article
-      }
+      delete article.categories;
+      return article
     });
   },
 };
